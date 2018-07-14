f9e74097fc1ab7c8717f011d08937cad86d00b8f
Michael Liu <michaelliu56@gmail.com>
2018-07-14 01:20:25 -0400

    new files

--- app/data/friend.js
+++ app/data/friend.js
@@ -16,7 +16,168 @@ var friendList = [
       "3",
       "4"
     ]
+  },
+  {
+    name: 'Mary',
+    profilePic: 'https://mliu0506.github.io/friends-finder/app/public/images/girl1.jpg',
+    scores: [
+      "4",
+      "1",
+      "2",
+      "3",
+      "4",
+      "1",
+      "1",
+      "2",
+      "3",
+      "1"
+    ]
+  },
+  {
+    name: 'Rose',
+    profilePic: 'https://mliu0506.github.io/friends-finder/app/public/images/girl2.jpg',
+    scores: [
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: 'Lisa',
+    profilePic: 'https://mliu0506.github.io/friends-finder/app/public/images/girl3.jpg',
+    scores: [
+      "4",
+      "3",
+      "5",
+      "2",
+      "1",
+      "5",
+      "4",
+      "1",
+      "3",
+      "1"
+    ]
+  },
+  {
+    name: 'Alison',
+    profilePic: 'https://mliu0506.github.io/friends-finder/app/public/images/girl4.jpg',
+    scores: [
+      "2",
+      "3",
+      "2",
+      "1",
+      "4",
+      "5",
+      "4",
+      "1",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: 'Ada',
+    profilePic: 'https://mliu0506.github.io/friends-finder/app/public/images/girl5.jpg',
+    scores: [
+      "1",
+      "1",
+      "2",
+      "1",
+      "2",
+      "3",
+      "1",
+      "1",
+      "1",
+      "3"
+    ]
+  },
+  {
+    name: 'Peter',
+    profilePic: 'https://mliu0506.github.io/friends-finder/app/public/images/boy1.jpg',
+    scores: [
+      "5",
+      "5",
+      "3",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: 'John',
+    profilePic: 'https://mliu0506.github.io/friends-finder/app/public/images/boy2.jpg',
+    scores: [
+      "2",
+      "1",
+      "1",
+      "3",
+      "4",
+      "4",
+      "1",
+      "4",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: 'Kyle',
+    profilePic: 'https://mliu0506.github.io/friends-finder/app/public/images/boy3.jpg',
+    scores: [
+      "3",
+      "2",
+      "1",
+      "2",
+      "1",
+      "5",
+      "4",
+      "4",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: 'Fankie',
+    profilePic: 'https://mliu0506.github.io/friends-finder/app/public/images/boy4.jpg',
+    scores: [
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: 'Mark',
+    profilePic: 'https://mliu0506.github.io/friends-finder/app/public/images/boy5.jpg',
+    scores: [
+      "1",
+      "5",
+      "1",
+      "2",
+      "1",
+      "5",
+      "4",
+      "4",
+      "3",
+      "1"
+    ]
   }
+
 ]
 
 module.exports = friendList;
